32a46c87656457bd68b0efd3a5b78f468c487061
Ali Maher <ali.maher.mohammed@gmail.com>
2022-12-04 07:55:26 +0200

    React with typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Heading from "./components/Heading"
+import Section from "./components/Section"
+
+function App() {
+  return (
+    <>
+      <Heading title={'Hello'} />
+      <Section>
+        This is my section
+      </Section>
+    </>
+  )
+}
+
+export default App
